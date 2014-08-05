c345a4d3ff56cbe10d2f5789c9df92c0a64e70cc
Ingvar Stepanyan <me@rreverser.com>
2014-08-05 21:01:47 +0300

    Tested :(

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // test
 Object.keys(process.env)
-.filter(RegExp.prototype.test.bind(/^TRAVIS_/)
+.filter(RegExp.prototype.test.bind(/^TRAVIS_/))
 .forEach(function (key) {
 	console.log(key, ':', process.env[key]);
 });
